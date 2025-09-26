daee6576e4135453bad6a806df782185b7753a70
King <24613526+R-D-King@users.noreply.github.com>
2025-09-26 03:09:26 +0300

    modified make_function
    make_fuction now creates functions files and appends "$argv", which
    enables passing arguments to the aliased commands.

--- fish/.config/fish/functions/make_function.fish
+++ fish/.config/fish/functions/make_function.fish
@@ -5,10 +5,13 @@
 #   make_function <new_function_name> '<commands_to_run_in_quotes>'
 #
 # Example:
-#   make_function lg 'lazygit'
+#   make_function ll 'ls -ahF'
 #
-# This will create a new file at ~/.config/fish/functions/lg.fish
-# making the 'lg' function available permanently.
+# This will create a new file at ~/.config/fish/functions/ll.fish
+# making the 'll' function available permanently.
+#
+# It automatically passes along any arguments you provide.
+# For example, `ll my_folder` will work as expected.
 
 function make_function
     # Check if both a name and a body for the function were provided.
@@ -22,6 +25,13 @@ function make_function
     set -l function_body $argv[2]
     set -l function_dir ~/.config/fish/functions
     set -l function_path $function_dir/$function_name.fish
+
+    # Automatically append '$argv' to pass arguments to the command,
+    # unless the user has already specified how to handle them.
+    # This makes creating simple aliases very easy.
+    if not string match --quiet -- "*\$argv*" "$function_body"
+        set function_body "$function_body \$argv"
+    end
 
     # Create the standard fish functions directory if it doesn't exist.
     if not test -d $function_dir
