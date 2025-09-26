656ff0e112a910e5f606183bfc1541adddebbcaf
King <24613526+R-D-King@users.noreply.github.com>
2025-09-26 03:24:14 +0300

    modified make_function to optionally take a description

--- fish/.config/fish/functions/make_function.fish
+++ fish/.config/fish/functions/make_function.fish
@@ -2,33 +2,30 @@
 # A utility function to create and save other fish functions.
 #
 # Usage:
-#   make_function <new_function_name> '<commands_to_run_in_quotes>'
+#   make_function <name> '<body>' ['<description>']
 #
-# Example:
-#   make_function ll 'ls -ahF'
+# Example with description:
+#   make_function ll 'ls -ahF' 'Lists files with details'
 #
 # This will create a new file at ~/.config/fish/functions/ll.fish
 # making the 'll' function available permanently.
-#
-# It automatically passes along any arguments you provide.
-# For example, `ll my_folder` will work as expected.
 
 function make_function
     # Check if both a name and a body for the function were provided.
     if test (count $argv) -lt 2
         echo "Error: Missing arguments."
-        echo "Usage: make_function <name> '<body>'"
+        echo "Usage: make_function <name> '<body>' ['<description>']"
         return 1
     end
 
     set -l function_name $argv[1]
     set -l function_body $argv[2]
+    set -l function_description $argv[3] # Optional third argument
     set -l function_dir ~/.config/fish/functions
     set -l function_path $function_dir/$function_name.fish
 
     # Automatically append '$argv' to pass arguments to the command,
     # unless the user has already specified how to handle them.
-    # This makes creating simple aliases very easy.
     if not string match --quiet -- "*\$argv*" "$function_body"
         set function_body "$function_body \$argv"
     end
@@ -39,8 +36,13 @@ function make_function
     end
 
     # Write the function definition to its own file.
-    # This makes the function permanent.
     echo "function $function_name" > $function_path
+    
+    # Add the description as a comment if it was provided.
+    if test -n "$function_description"
+        echo "    # $function_description" >> $function_path
+    end
+
     echo "    $function_body" >> $function_path
     echo "end" >> $function_path
 
